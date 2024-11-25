b2dd3fae27e63fe859e4c6210470d6b50dc854f0
phjain <phjain@systango.com>
2024-11-25 11:44:15 +0530

    added banner

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,20 @@
 import { useTranslations } from 'next-intl';
 import { Link } from '@/i18n/routing';
+import Image from 'next/image';
 
 function App() {
   const t = useTranslations('HomePage');
   return (
-    <div>
-      <h1>{t('title')}</h1>
-      <Link href="/about">{t('about')}</Link>
+    <div className='container mx-auto'>
+      <Image
+        src="/images/banner.png"
+        alt="banner img"
+        width={1360}
+        height={100}
+        className="w-100 h-100 mt-5"
+      />
+      {/* <h1>{t('title')}</h1>
+      <Link href="/about">{t('about')}</Link> */}
     </div>
   );
 }
